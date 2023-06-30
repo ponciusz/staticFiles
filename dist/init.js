a3d93d40af241d901e1c1040bd8a2bfe2dd5c324
Kamil Albrycht <ponciusz@gmail.com>
2023-06-30 23:38:47 +0100

    adding dist to gitignore

---
+++ dist/init.js
@@ -0,0 +1,28 @@
+var wrap = document.createElement("div");
+wrap.id = "debug-tools";
+document.body.appendChild(wrap);
+
+const scriptUrl = "https://custom-debug-kamil.surge.sh/assets/index-ae17c913.js";
+const styleUrl = "https://custom-debug-kamil.surge.sh/assets/index-578c6832.css";
+
+fetch(scriptUrl)
+  .then((response) => response.text())
+  .then((scriptCode) => {
+    const scriptTag = document.createElement("script");
+    scriptTag.text = scriptCode;
+    document.head.appendChild(scriptTag);
+  })
+  .catch((error) => {
+    console.error("Error with injecting script", error);
+  });
+
+fetch(styleUrl)
+  .then((response) => response.text())
+  .then((css) => {
+    const style = document.createElement("style");
+    style.textContent = css;
+    document.head.append(style);
+  })
+  .catch((error) => {
+    console.error("Error with injecting styles:", error);
+  });
